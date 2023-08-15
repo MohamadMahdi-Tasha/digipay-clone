1c942cd40fe26058334632b97d6cc76f664105b0
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-15 16:09:36 +0330

    added styles of last commit

--- components/collectHeaderComponent.tsx
+++ components/collectHeaderComponent.tsx
@@ -18,17 +18,17 @@ interface propsType {
 export default function CollectHeaderComponent({isOpened, onMouseEnter, onMouseLeave}:propsType):React.ReactNode {
     // Returning JSX
     return (
-        <div data-opened={isOpened} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className={'border-t transition-all duration-500 border-t-myYellow shadow-lg h-[80vh] overflow-auto lg:block hidden data-[opened="true"]:visible data-[opened="true"]:opacity-100 data-[opened="false"]:invisible data-[opened="false"]:h-[0] data-[opened="false"]:opacity-0'}>
-            <div>
-                <div>
-                    <span>دریافت وام ۵۰ ملیون تومانی</span>
-                    <p> تا 5۰ میلیون تومان اعتبار بعد از ثبت نام در دیجی پی  برای خرید اقساطی کالا از بین 10 میلیون کالا متنوع از برترین برند ها در بزرگترین فروشگاه اینترنتی دیجی کالا </p>
-                    <a href="#">
+        <div data-opened={isOpened} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className={'border-t transition-all duration-500 border-t-myYellow shadow-lg h-[50vh] overflow-auto lg:block hidden data-[opened="true"]:visible data-[opened="true"]:opacity-100 data-[opened="false"]:invisible data-[opened="false"]:h-[0] data-[opened="false"]:opacity-0'}>
+            <div className={'flex gap-[20px] p-[20px]'}>
+                <div className={'w-[75%]'}>
+                    <span className={'font-bold text-[24px] mb-[10px] text-darkBlue'}>دریافت وام ۵۰ ملیون تومانی</span>
+                    <p className={'font-light text-[16px] mb-[20px] text-darkBlue/80'}> تا 5۰ میلیون تومان اعتبار بعد از ثبت نام در دیجی پی  برای خرید اقساطی کالا از بین 10 میلیون کالا متنوع از برترین برند ها در بزرگترین فروشگاه اینترنتی دیجی کالا </p>
+                    <a className={'text-themeColor flex whitespace-nowrap gap-[10px]'} href="#">
                         دریافت اعتبار
                         <IconComponent name={'left-arrow'} />
                     </a>
                 </div>
-                <Image src={AqsatiImage.src} alt={'خرید اقساطی'} width={100} height={100} />
+                <Image className={'w-[25%] object-cover'} src={AqsatiImage.src} alt={'خرید اقساطی'} width={100} height={100} />
             </div>
             <ServicesHeaderFooterComponent />
         </div>
