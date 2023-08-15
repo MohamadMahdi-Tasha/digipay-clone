151d40b8218fb8526c6faa53870bddd10aaf7b6b
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-15 17:02:52 +0330

    added styles of last commit

--- components/onlineMagazineHeaderComponent.tsx
+++ components/onlineMagazineHeaderComponent.tsx
@@ -31,46 +31,46 @@ export default function OnlineMagazineHeaderComponent({isOpened, onMouseEnter, o
                     <a className={'truncate block text-darkBlue/80 font-light text-[16px] transition-all hover:text-themeColor'} href="#">پی کست</a>
                     <a className={'truncate block text-darkBlue/80 font-light text-[16px] transition-all hover:text-themeColor'} href="#">راهنمای خرید اقساطی</a>
                 </div>
-                <div className={'w-[75%] p-[20px]'}>
-                    <div>
-                        <Image src={BlogImage1} alt={'بانکداری دیجیتال در خاورمیانه'} />
-                        <div>
-                            <span>بانکداری دیجیتال در خاورمیانه</span>
-                            <span>
+                <div className={'w-[75%] p-[20px] grid grid-cols-2 gap-[20px]'}>
+                    <div className={'flex items-center gap-[10px]'}>
+                        <Image width={100} height={100} className={'w-[40%] h-[100px] object-cover rounded-[20px]'} src={BlogImage1} alt={'بانکداری دیجیتال در خاورمیانه'} />
+                        <a href={'#'} className={'w-[60%] block'}>
+                            <span className={'font-bold text-[16px] text-darkBlue mb-[10px] truncate block'}>بانکداری دیجیتال در خاورمیانه</span>
+                            <span className={'flex items-center gap-[10px] text-[14px] text-darkBlue/80 font-light truncate'}>
                                 <IconComponent name={'calender'} />
                                 ۶ ماه پیش
                             </span>
-                        </div>
+                        </a>
                     </div>
-                    <div>
-                        <Image src={BlogImage2} alt={'شرایط خرید سرویس اعتباری (BNPL) در دنیا چگونه است؟'} />
-                        <div>
-                            <span>شرایط خرید سرویس اعتباری (BNPL) در دنیا چگونه است؟</span>
-                            <span>
+                    <div className={'flex items-center gap-[10px]'}>
+                        <Image width={100} height={100} className={'w-[40%] h-[100px] object-cover rounded-[20px]'} src={BlogImage2} alt={'شرایط خرید سرویس اعتباری (BNPL) در دنیا چگونه است؟'} />
+                        <a href={'#'} className={'w-[60%] block'}>
+                            <span className={'font-bold text-[16px] text-darkBlue mb-[10px] truncate block'}>شرایط خرید سرویس اعتباری (BNPL) در دنیا چگونه است؟</span>
+                            <span className={'flex items-center gap-[10px] text-[14px] text-darkBlue/80 font-light truncate'}>
                                 <IconComponent name={'calender'} />
                                 ۷ ماه پیش
                             </span>
-                        </div>
+                        </a>
                     </div>
-                    <div>
-                        <Image src={BlogImage3} alt={'۱۷ عادت مالی هوشمند که همه باید بدانند'} />
-                        <div>
-                            <span>۱۷ عادت مالی هوشمند که همه باید بدانند</span>
-                            <span>
+                    <div className={'flex items-center gap-[10px]'}>
+                        <Image width={100} height={100} className={'w-[40%] h-[100px] object-cover rounded-[20px]'} src={BlogImage3} alt={'۱۷ عادت مالی هوشمند که همه باید بدانند'} />
+                        <a href={'#'} className={'w-[60%] block'}>
+                            <span className={'font-bold text-[16px] text-darkBlue mb-[10px] truncate block'}>۱۷ عادت مالی هوشمند که همه باید بدانند</span>
+                            <span className={'flex items-center gap-[10px] text-[14px] text-darkBlue/80 font-light truncate'}>
                                 <IconComponent name={'calender'} />
                                 ۷ ماه پیش
                             </span>
-                        </div>
+                        </a>
                     </div>
-                    <div>
-                        <Image src={BlogImage4} alt={'روش هایمدیریت مالی برای استارتاپ های نوپا'} />
-                        <div>
-                            <span>روش هایمدیریت مالی برای استارتاپ های نوپا</span>
-                            <span>
+                    <div className={'flex items-center gap-[10px]'}>
+                        <Image width={100} height={100} className={'w-[40%] h-[100px] object-cover rounded-[20px]'} src={BlogImage4} alt={'روش هایمدیریت مالی برای استارتاپ های نوپا'} />
+                        <a href={'#'} className={'w-[60%] block'}>
+                            <span className={'font-bold text-[16px] text-darkBlue mb-[10px] truncate block'}>روش هایمدیریت مالی برای استارتاپ های نوپا</span>
+                            <span className={'flex items-center gap-[10px] text-[14px] text-darkBlue/80 font-light truncate'}>
                                 <IconComponent name={'calender'} />
                                 ۷ ماه پیش
                             </span>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </HolderComponent>
