51437f2e3197bce3a08111cb1851f66e891de392
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-16 14:44:51 +0330

    added second section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@
 // Importing Part
 import React from "react";
 import FirstSectionComponent from '@/components/pageSections/firstSectionComponent';
+import SecondSectionComponent from "@/components/pageSections/secondSectionComponent";
 
 // Creating Home Page And Exporting It As Default
 export default function HomePage(): React.ReactNode {
     // Returning JSX
     return (
-        <FirstSectionComponent/>
+        <>
+            <FirstSectionComponent/>
+            <SecondSectionComponent/>
+        </>
     );
 }
