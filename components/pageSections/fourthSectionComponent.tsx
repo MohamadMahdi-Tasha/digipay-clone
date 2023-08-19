d2828eabf6038d34746201c5e125579a10f12046
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-19 13:11:28 +0330

    added mqs of fourth section

--- components/pageSections/fourthSectionComponent.tsx
+++ components/pageSections/fourthSectionComponent.tsx
@@ -22,20 +22,23 @@ export default function FourthSectionComponent(): React.ReactNode {
                     <h5 className={'text-center text-darkBlue/80 text-[24px] mb-[40px] font-light'}>جدیدترین اخبار و مطالب حوزه مالی، پرداخت و فین‌تک</h5>
                 </header>
                 <main>
-                    <div className={'flex items-center gap-[20px] mb-[30px]'}>
-                        <div className={'w-[85%] flex justify-start items-center gap-[20px] overflow-auto'}>
+                    <div className={'flex lg:flex-row flex-col items-center gap-[20px] mb-[30px]'}>
+                        <div className={'lg:w-[85%] flex justify-start items-center gap-[20px] overflow-auto'}>
                             <button className={'flex-shrink-0 border border-darkBlue text-darkBlue text-[16px] font-normal px-[20px] py-[10px] rounded-[50rem]'}>دنیای مالی</button>
                             <button className={'flex-shrink-0 border border-darkBlue text-darkBlue text-[16px] font-normal px-[20px] py-[10px] rounded-[50rem]'}>زندگی هوشمند</button>
                             <button className={'flex-shrink-0 border border-darkBlue text-darkBlue text-[16px] font-normal px-[20px] py-[10px] rounded-[50rem]'}>اتاق خبر دیجی پی</button>
                             <button className={'flex-shrink-0 border border-darkBlue text-darkBlue text-[16px] font-normal px-[20px] py-[10px] rounded-[50rem]'}>پی کست</button>
                             <button className={'flex-shrink-0 border border-darkBlue text-darkBlue text-[16px] font-normal px-[20px] py-[10px] rounded-[50rem]'}>راهمای خرید اقساطی</button>
                         </div>
-                        <div className={'w-[15%] flex justify-end items-center gap-[10px]'}>
+                        <div className={'w-[15%] lg:flex hidden justify-end items-center gap-[10px]'}>
                             <button className={'w-[30px] h-[30px] flex justify-center items-center'}><IconComponent name={'chevron-right'} /></button>
                             <button className={'w-[30px] h-[30px] flex justify-center items-center'}><IconComponent name={'chevron-left'} /></button>
                         </div>
                     </div>
-                    <Swiper className={'fourth-section-slider'} slidesPerView={2} spaceBetween={50}>
+                    <Swiper className={'fourth-section-slider'} spaceBetween={50} breakpoints={{
+                        991: {slidesPerView: 2,},
+                        0: {slidesPerView: 1,}
+                    }}>
                         <SwiperSlide><BlogComponent img={BlogImage1.src} title={'بهترین گوشی های هوشمند از نظر باتری'} type={'راهنمای خرید اقساطی'} date={'8/17/2023'} /></SwiperSlide>
                         <SwiperSlide><BlogComponent img={BlogImage1.src} title={'بهترین گوشی های هوشمند از نظر باتری'} type={'راهنمای خرید اقساطی'} date={'8/17/2023'} /></SwiperSlide>
                         <SwiperSlide><BlogComponent img={BlogImage1.src} title={'بهترین گوشی های هوشمند از نظر باتری'} type={'راهنمای خرید اقساطی'} date={'8/17/2023'} /></SwiperSlide>
